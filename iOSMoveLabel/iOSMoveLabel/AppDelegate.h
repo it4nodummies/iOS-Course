098708f6cdceed682e597fc36e74f2e1a405007a
it4nodummies <ivano.turi@me.com>
2013-11-13 15:10:35 +0100

    First Commit for setup

---
+++ iOSMoveLabel/iOSMoveLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSMoveLabel
+//
+//  Created by Ivano Turi on 13/11/13.
+//  Copyright (c) 2013 Ivano Turi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSMoveLabel/iOSMoveLabel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSMoveLabel
+//
+//  Created by Ivano Turi on 13/11/13.
+//  Copyright (c) 2013 Ivano Turi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
