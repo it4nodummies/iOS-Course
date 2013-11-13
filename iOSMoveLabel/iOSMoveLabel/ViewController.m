93a85572a00d0707d299c18c73008f5a8c0d2050
it4nodummies <ivano.turi@me.com>
2013-11-13 17:28:23 +0100

    implementazione slider che cambia Alpha alla label

--- iOSMoveLabel/iOSMoveLabel/ViewController.m
+++ iOSMoveLabel/iOSMoveLabel/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *lblAlpha;
+@property (weak, nonatomic) IBOutlet UILabel *lblAlphaValue;
+@property (strong, nonatomic) NSDictionary *dctOffsetMoving;
 
 @end
 
@@ -18,12 +21,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.lblAlphaValue setText:[NSString stringWithFormat:@"%.2f", [self.lblAlpha alpha] ]];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sldChangeAlphaLabel:(UISlider *)sender {
+    [self.lblAlpha setAlpha:[sender value]];
+    [self.lblAlphaValue setText:[NSString stringWithFormat:@"%.2f", [self.lblAlpha alpha]]];
+    
 }
 
 @end
