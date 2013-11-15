6fa6f244de17ab203f4b12d27bdffaf690f53ca0
it4nodummies <ivano.turi@me.com>
2013-11-15 18:26:06 +0100

    prima struttura app

---
+++ iOSContacts/iOSContacts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSContacts
+//
+//  Created by Ivano Turi on 15/11/13.
+//  Copyright (c) 2013 Ivano Turi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSContacts/iOSContacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSContacts
+//
+//  Created by Ivano Turi on 15/11/13.
+//  Copyright (c) 2013 Ivano Turi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
